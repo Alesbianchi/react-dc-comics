9d953d9d7fc0ef173d189ffd1197ea4d4bc98bb2
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-02-12 16:53:37 +0100

    add main component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
-// App.jsx
+// importo header
 import Header from "./components/Header.jsx";
+
+// importo header
+import Main from "./components/Main.jsx";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
